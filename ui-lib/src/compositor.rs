0a5c9f1c5fff5ed26d57d16b2cd3ec2f4c8fd637
PL <phat.luong@codeforcevina.com>
2026-01-23 17:49:02 +0700

    compositor

--- ui-lib/src/compositor.rs
+++ ui-lib/src/compositor.rs
@@ -1,7 +1,7 @@
 use app_file_manager::{FileManager, FileManagerMessage};
 use iced::{
-    Element, Size,
-    widget::{container, row, scrollable, text},
+    Alignment, Element, Length, Size,
+    widget::{column, container, row, scrollable, text},
 };
 
 use crate::{ApplicationItem, ApplicationType};
@@ -28,15 +28,26 @@ impl Compositor {
     pub fn view(&self, widown_size: Size) -> Element<'_, CompositorMessage> {
         let windows = scrollable(
             row(self.open_apps.iter().map(|app| match app.app_type {
-                ApplicationType::FilesManager => {
-                    container(self.file_manager.view().map(CompositorMessage::FileManager))
-                        .width(widown_size.width / 2.0)
-                        .height(widown_size.height / 1.3)
-                        .into()
-                }
+                ApplicationType::FilesManager => container(column![
+                    container(text("FileManager"))
+                        .padding(8)
+                        .width(Length::Fill)
+                        .style(|_| container::Style {
+                            background: Some(iced::Background::Color(iced::Color::from_rgb8(
+                                167, 186, 186
+                            ))),
+                            ..Default::default()
+                        }),
+                    self.file_manager.view().map(CompositorMessage::FileManager)
+                ])
+                .width((widown_size.width - 12.0) / 2.0)
+                .height(widown_size.height / 1.3)
+                .into(),
                 _ => container(text("Not support")).into(),
             }))
-            .spacing(12),
+            .spacing(12)
+            .height(Length::Fill)
+            .align_y(Alignment::Center),
         )
         .horizontal();
 
